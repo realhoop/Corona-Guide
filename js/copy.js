eb672fc271e3ffc6622c102061c218bd863f4bea
Keunhee Cho <98001670+realhoop@users.noreply.github.com>
2022-02-27 15:18:23 +0900

    ver.1.0 (2022.02.27)

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+function copyToClipboard(val) {
+    const t = document.createElement("textarea");
+    document.body.appendChild(t);
+    t.value = val;
+    t.select();
+    document.execCommand('copy');
+    document.body.removeChild(t);
+  }
+  function copy(link) {
+    copyToClipboard(link);
+    alert('Copied!');
+  }
